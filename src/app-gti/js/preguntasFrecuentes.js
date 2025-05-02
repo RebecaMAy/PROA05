3b3c96580f39db1020b6208703502b877b1dccce
ALANGMupv <alanguevara36@gmail.com>
2025-05-02 22:58:56 +0200

    chore: últimos pequeños cambios

--- src/app-gti/js/preguntasFrecuentes.js
+++ src/app-gti/js/preguntasFrecuentes.js
@@ -1,3 +1,14 @@
+// Código para que no se queden las preguntas abiertas al volver a la página
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.faq-respuesta').forEach(respuesta => {
+        respuesta.classList.remove('activa');
+    });
+    document.querySelectorAll('.faq-icono').forEach(icono => {
+        icono.style.transform = 'rotate(0deg)';
+    });
+});
+
+// Lógica FAQ
 document.querySelectorAll('.faq-pregunta').forEach(button => {
     button.addEventListener('click', () => {
         const respuesta = button.nextElementSibling;
